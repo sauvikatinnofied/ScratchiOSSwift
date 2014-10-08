e69c6015e2f080428dd4914787ce86367efced60
Sauvik Dolui <sauvik.dolui@innofied.com>
2014-10-08 10:39:49 +0530

    Initial Commit

---
+++ ScratchiOSSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScratchiOSSwift
+//
+//  Created by Sauvik Dolui on 10/8/14.
+//  Copyright (c) 2014 Innofied Solution Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
